bcc0fd9e7dbba2b3b2838dfd8dd091a1a5c33df1
shweta_garg <shwetagarg2006@gmail.com>
2014-08-21 19:08:44 +0530

    added code of anagrams

---
+++ anagrams_from_list_of_words.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+import sys
+
+def get_anagrams_dict(words):
+	anagrams_dict = defaultdict(list)
+	for word in words:
+		word = word.rstrip()
+		key = ''.join(sorted(word))
+		anagrams_dict[key].append(word)
+	return anagrams_dict
+		
+def get_anagrams_list(anagrams_dict):
+	anagrams_list = []	
+	for key in anagrams_dict:
+		anagrams_list.append(anagrams_dict[key])
+	return anagrams_list
+		
+def get_anagrams(words):
+	anagrams_dict = get_anagrams_dict(words)
+	return get_anagrams_list(anagrams_dict)
+
+def main():
+	words = ['cat', 'tac', 'mat', 'rat', 'tam', 'act']
+	anagrams_list = get_anagrams(words)
+	print anagrams_list
+
+if __name__ == "__main__":
+    main()
