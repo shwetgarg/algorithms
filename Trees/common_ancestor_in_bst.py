21fa3abe833f42e960d1ec7af18eb6987ba6e9b3
Shweta Garg <shweta@indix.com>
2016-02-17 11:48:44 +0530

    few simplifications

--- Trees/common_ancestor_in_bst.py
+++ Trees/common_ancestor_in_bst.py
@@ -1,28 +1,31 @@
 from tree import *
 
-class CommonAncestor(Tree):
-    def get_common_ancestor(self, v1, v2):
-        if not self:
-            return
-        
-        if self.v < v1 and self.v < v2:
-            if self.r:	return self.r.get_common_ancestor(v1, v2)
-        elif self.v > v1 and self.v > v2:
-            if self.l: return self.l.get_common_ancestor(v1, v2)
-        else: return self	
-        
+
+def get_common_ancestor(tree, v1, v2):
+    if not tree:
+        return
+
+    if tree.v < v1 and tree.v < v2:
+        if tree.r: return get_common_ancestor(tree.r, v1, v2)
+    elif tree.v > v1 and tree.v > v2:
+        if tree.l: return get_common_ancestor(tree.l, v1, v2)
+    else:
+        return tree
+
+
 def main():
-    a = CommonAncestor(1)
-    b = CommonAncestor(4)
-    c = CommonAncestor(6)
-    d = CommonAncestor(8)
-    e = CommonAncestor(2, a)
-    f = CommonAncestor(3, e, b)
-    g = CommonAncestor(7, c, d)
-    h = CommonAncestor(5, f, g)
-    
-    node = h.get_common_ancestor(1, 4)
-    if node: print node.v 
-    
+    a = Tree(1)
+    b = Tree(4)
+    c = Tree(6)
+    d = Tree(8)
+    e = Tree(2, a)
+    f = Tree(3, e, b)
+    g = Tree(7, c, d)
+    h = Tree(5, f, g)
+
+    node = get_common_ancestor(h, 1, 4)
+    if node: print node.v
+
+
 if __name__ == "__main__":
     main()
